a6bb916bb655d24d38da0f5923e6df8ad324e700
deathcrush <reidvai1998@gmail.com>
2019-11-08 00:43:04 +0600

    created: api services

---
+++ client/src/services/profile_service.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+import axios from 'axios'
+
+const baseURI = 'http://localhost:5000/resume'
+
+export async function getResume(){
+  const auth_token = localStorage.token;
+  if(!auth_token) return {error: 'Access Denied'};
+  const res =  await axios.get(`${baseURI}/`, {headers: { auth_token }});
+  console.log(res);
+  return res.data;
+}
---
+++ client/src/services/user_services.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+import axios from 'axios'
+
+const baseURI = 'http://localhost:5000/user'
+
+export async function regUser(user){
+  try {
+    const res = await axios.post(`${baseURI}/register`, user);
+    return {_id: res.data._id};
+  } catch (error) {
+    return {error: 'Email already exists'};
+  }
+}
+
+export async function signUser(user){
+  try {
+    const res = await axios.post(`${baseURI}/login`, user);
+    localStorage.token = res.data.token;
+    return { token: res.data.token };
+  } catch (error) {
+    return { error: 'Email or Password is invalid' };
+  }
+}
