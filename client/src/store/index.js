d697376d3773daea715c0928e8688d0c764cf359
deathcrush <reidvai1998@gmail.com>
2019-11-08 15:37:58 +0600

    added: store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,10 +5,32 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: null,
+    resume: null
+  },
+  getters:{
+    getUser: state => {
+      return state.user;
+    },
+    getResume: state => {
+      return state.resume;
+    }
   },
   mutations: {
+    UPDATE_RESUME: (state, resume) => {
+      state.resume = resume;
+    },
+    UPDATE_USER: (state, user) => {
+      state.user = user;
+    }
   },
   actions: {
+    updateResume: (context, resume) => {
+      context.commit('UPDATE_RESUME', resume);
+    },
+    updateUser: (context, user) => {
+      context.commit('UPDATE_USER', user);
+    }
   },
   modules: {
   }
