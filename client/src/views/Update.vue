6c278eb7aea1787b65d557c131386777dd386f90
deathcrush <reidvai1998@gmail.com>
2019-11-11 18:29:42 +0600

    added: updating resume

---
+++ client/src/views/Update.vue
@@ -0,0 +1,15 @@
+<template>
+  <transition name="drawer">
+    <ResumeForm />
+  </transition>
+</template>
+
+<script>
+import ResumeForm from '../components/ResumeForm'
+
+export default {
+  components:{
+    ResumeForm
+  }
+}
+</script>
